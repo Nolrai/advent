4f3eab099f4a3dc418d2e41e2eba389751b0318f
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-12-15 09:28:20 -0800

    sovles part 1 of day 1

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@
 
 module Main (main) where
 import Conduit
-import Data.List as L
 import Data.Conduit.Text
 import Data.Conduit.List (mapAccum)
 import Text.Megaparsec
@@ -11,27 +10,19 @@ import qualified Text.Megaparsec.Char.Lexer as L
 
 main :: IO ()
 main = do
-  [window', inputFile] <- getArgs
-  let Just window = parseDecimal (pack window')
-  (len :: Int) 
-    <- runConduitRes $
-      sourceFile inputFile 
-        .| decodeUtf8C 
-        .| linesBounded 100
-        .| mapWhileC parseDecimal
-  --      .| slidingAverage window
-        .| pairUp
-        .| lengthIfC (uncurry (<))
-  print len
+  [inputFile] <- getArgs
+  runConduitRes $
+    sourceFile inputFile 
+      .| decodeUtf8C 
+      .| linesBounded 100 
+      .| mapWhileC parseDecimal
+      .| pairUp
+      .| do
+        (len :: Int) <- lengthIfC (uncurry (<)) 
+        liftIO $ print len
 
-slidingAverage :: Monad m => Int -> ConduitT Int Int m Int
-slidingAverage window =
-  mapAccum step [] .| filterC ((== window) . L.length) .| mapC L.sum
-  where
-    step new l = let l' = new : L.take (window - 1) l in (l', l')
-
-pairUp ::  Monad m => ConduitT Int (Int, Int) m Int
-pairUp = mapAccum step maxBound
+pairUp :: ConduitT Int (Int, Int) (ResourceT IO) ()
+pairUp = mapAccum step maxBound >> pure ()
   where
     step :: Int -> Int -> (Int, (Int, Int))
     step new old = (new, (old, new))
