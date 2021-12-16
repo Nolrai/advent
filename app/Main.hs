9c8fe2611a3d493a64b6b801b6d1653d96c7744f
Chris Upshaw (From Yukari) <chrisaupshaw@gmail.com>
2021-12-15 19:44:59 -0800

    day one solved

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
+import qualified Data.List as P -- P for Prelude
 import Conduit
 import Data.Conduit.Text
 import Data.Conduit.List (mapAccum)
@@ -10,16 +11,27 @@ import qualified Text.Megaparsec.Char.Lexer as L
 
 main :: IO ()
 main = do
-  [inputFile] <- getArgs
+  [window', inputFile] <- getArgs
+  let Just window = parseDecimal (fromString window')
   runConduitRes $
     sourceFile inputFile 
       .| decodeUtf8C 
       .| linesBounded 100 
       .| mapWhileC parseDecimal
+      .| slidingWindow window
       .| pairUp
       .| do
         (len :: Int) <- lengthIfC (uncurry (<)) 
         liftIO $ print len
+
+slidingWindow :: Int -> ConduitT Int Int (ResourceT IO) ()
+slidingWindow window = getWindow .| step2 .| step3
+  where
+    getWindow = mapAccum getWindowHelper [] >> pure ()
+    step2 = filterC ((== window) . P.length)
+    step3 = mapC P.sum
+    getWindowHelper :: Int -> [Int] -> ([Int], [Int])
+    getWindowHelper new soFar = let r = P.take window (new : soFar) in (r, r)
 
 pairUp :: ConduitT Int (Int, Int) (ResourceT IO) ()
 pairUp = mapAccum step maxBound >> pure ()
